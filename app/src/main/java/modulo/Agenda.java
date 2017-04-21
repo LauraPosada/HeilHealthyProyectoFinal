83735ad43a63ed17cdbd3cd6b0caafa839d3c9e2
OscarFabian <oscar_1149@hotmail.es>
2017-04-20 20:00:39 -0500

    Clases BD

--- app/src/main/java/modulo/Agenda.java
+++ app/src/main/java/modulo/Agenda.java
@@ -1,5 +1,6 @@
 package modulo;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -10,7 +11,55 @@ public class Agenda {
 
     private int id;
     private int medico_id;
-    //private fecha;
-    //private hora_inicio;
-    //private hora_final;
+    private Calendar fecha;
+    private Calendar hora_inicio;
+    private Calendar hora_final;
+
+    public Agenda(int id, int medico_id, Calendar fecha, Calendar hora_inicio, Calendar hora_final) {
+        this.id = id;
+        this.medico_id = medico_id;
+        this.fecha = fecha;
+        this.hora_inicio = hora_inicio;
+        this.hora_final = hora_final;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMedico_id() {
+        return medico_id;
+    }
+
+    public void setMedico_id(int medico_id) {
+        this.medico_id = medico_id;
+    }
+
+    public Calendar getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Calendar fecha) {
+        this.fecha = fecha;
+    }
+
+    public Calendar getHora_inicio() {
+        return hora_inicio;
+    }
+
+    public void setHora_inicio(Calendar hora_inicio) {
+        this.hora_inicio = hora_inicio;
+    }
+
+    public Calendar getHora_final() {
+        return hora_final;
+    }
+
+    public void setHora_final(Calendar hora_final) {
+        this.hora_final = hora_final;
+    }
 }
