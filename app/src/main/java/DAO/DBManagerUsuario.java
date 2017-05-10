d036198e9a057640b57769ea46b42edec5aacb97
OscarFabian <oscar_1149@hotmail.es>
2017-05-10 18:54:40 -0500

    lokas .!.

---
+++ app/src/main/java/DAO/DBManagerUsuario.java
@@ -0,0 +1,14 @@
+package DAO;
+
+import android.database.Cursor;
+
+/**
+ * Created by oscar on 10/05/17.
+ */
+
+public class DBManagerUsuario {
+
+    public Cursor ListarPaciente(){
+        return null;
+    }
+}
